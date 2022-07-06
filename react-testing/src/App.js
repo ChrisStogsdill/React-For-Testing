0da84d9e0b50d9165e1f766e95b8b0558e2a7635
ChrisStogsdill <chris1stogsdill@gmail.com>
2022-07-06 15:54:07 -0500

    Fill in some of the Router Lesson

--- react-testing/src/App.js
+++ react-testing/src/App.js
@@ -1,47 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
-import React, { useState, useEffect } from 'react';
-// test change to make sure commit works
-function App() {
-  const [color, setColor] = useState("black");
-
-  useEffect(() => {
-    function changeColorOnClick(event) {
-      if (color === "black") {
-        setColor("red");
-      }
-      else {
-        setColor("black")
-      }
-    
-    };
-
-    document.addEventListener("click", changeColorOnClick);
-
-    return () => {
-      document.removeEventListener("click", changeColorOnClick );
-    };
-  }, [color]);
 
 
+const App = () => {
   return (
     <div>
-            <div
-        id="myDiv"
-        style={{
-          color: "white",
-          width: "100px",
-          height: "100px",
-          position: "absolute",
-          left: "50%",
-          top: "50%",
-          backgroundColor: color,
-        }}
-      >
-        This div can change color. Click on me!
-      </div>
+      <h1>Hello from App</h1>
     </div>
   );
-}
+};
+
+
+  
 
 export default App;
